2c2a37a0f0d11977eba425669d4f6d2cf8e75f20
ydfdas1f546g1df <niklauskuelling@gmx.ch>
2024-05-23 16:40:57 +0200

    Automation Backup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,18 +2,33 @@ return {
   -- Plugin management via Mason for easy LSP installation
   {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
-    -- "mfussenegger/nvim-lint",
-    -- "jose-elias-alvarez/null-ls.nvim",
-    "nvimtools/none-ls.nvim",
-    -- "mhartington/formatter.nvim",
     config = function()
       require("mason").setup({
         ensure_installed = {},
       })
     end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  {
+
+    "neovim/nvim-lspconfig",
+  },
+  {
+
+    "mfussenegger/nvim-dap",
+  },
+  {
+
+    "rcarriga/nvim-dap-ui",
+  },
+  {
+
+    -- "mfussenegger/nvim-lint",
+    -- "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
+
+    -- "mhartington/formatter.nvim",
   },
 }
